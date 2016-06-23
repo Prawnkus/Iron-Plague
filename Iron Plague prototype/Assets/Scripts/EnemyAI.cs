c4862c1ecac36949f00f12f254c4d2a8b8e287e6
WishPotato <wish92jfn@hotmail.com>
2016-06-23 17:28:51 +0200

    Setup for EnemyAI
    Basic function and variables has been setup.

---
+++ Iron Plague prototype/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    private bool canSeeTarget, isAimingAt;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+
+    private void ShootAtTarget(Transform target)
+    {
+
+    }
+
+    private void AimTowardsTarget(Transform target)
+    {
+
+    }
+
+    private void MoveToLocation(Vector3 selectedLocation)
+    {
+
+    }
+}
