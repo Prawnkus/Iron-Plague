427409ac8c271dc94af0353da081171710f03800
WishPotato <wish92jfn@hotmail.com>
2016-06-25 21:31:07 +0200

    EnemyAI code setup
    The code has now been setup, and now needs to be tested.
    Currently does:
    Walkes between two location, until it can see the player. When It sees
    the player, it will hunt the player, and try to shoot at them.

--- Iron Plague prototype/Assets/Scripts/EnemyAI.cs
+++ Iron Plague prototype/Assets/Scripts/EnemyAI.cs
@@ -3,36 +3,49 @@ using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
 
-    private bool canSeeTarget, isAimingAt;
+    private float speed, rotationSpeed;
+    private bool canSeeTarget, isAimingAt, hasSeenPlayer;
     private RaycastHit hit, playerHit;
     private Vector3 direction;
+    private Transform direct;
 
     private Rigidbody rb;
+    [SerializeField]
     private GameObject player;
+    [SerializeField]
+    private GameObject passiveWalkLocation1, passiveWalkLocation2;
 
     void Start()
     {
+        speed = 5.0f;
+        rotationSpeed = 3.0f;
         rb = transform.GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void FixedUpdate()
     {
-        direction = player.transform.position - this.transform.position;
-
-        if (Angle(direction) < 10.0f)
-            canSeeTarget = true;
 
         if (!IsTooFarAway(player.transform) && CanSeeTarget(player.transform))
             ShootAtTarget(player.transform);
 
         if(Physics.Raycast(this.transform.position, player.transform.position, out playerHit))
         {
-            if (playerHit.collider.gameObject.tag == "Player")
-                AimTowardsTarget(player.transform);
-            else if (playerHit.collider.gameObject.tag != "Player")
+            if (playerHit.collider.gameObject.tag == "Player" && !IsTooFarAway(player.transform))
+                hasSeenPlayer = true;
+
+            if (hasSeenPlayer)
             {
-                // Move around to a random location
+                AimTowardsTarget(player.transform);
+                if (IsTooFarAway(player.transform))
+                    rb.MovePosition(this.transform.position + this.transform.forward * speed * Time.fixedDeltaTime);
+                else
+                    ShootAtTarget(player.transform);
+            }
+            else if (!hasSeenPlayer)
+            {
+                PassivePathing(direction, passiveWalkLocation1.transform, passiveWalkLocation1.transform);
+                AimTowardsTarget(direct);
             }
         }
     }
@@ -45,14 +58,11 @@ public class EnemyAI : MonoBehaviour {
 
     private void AimTowardsTarget(Transform target)
     {
-        
-    }
-
-    private void MoveToLocation(Vector3 selectedLocation)
-    {
-        if (IsTooFarAway(player.transform))
+        if (!CanSeeTarget(target))
         {
-
+            float angle = Mathf.Atan2(Direction(target).y, Direction(target).x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.fixedDeltaTime * rotationSpeed);
         }
     }
 
@@ -60,7 +70,7 @@ public class EnemyAI : MonoBehaviour {
     {
         bool canSee = false;
         if (Angle(direction) < 10.0f && Physics.Raycast(this.transform.position, this.transform.forward, out hit))
-            if (Vector3.Distance(this.transform.position, hit.collider.gameObject.transform.position) >= Vector3.Distance(this.transform.position, target.transform.forward))
+            if (Vector3.Distance(this.transform.position, hit.collider.gameObject.transform.position) >= Vector3.Distance(this.transform.position, target.forward))
                 canSee = true;
         return canSee;
     }
@@ -77,5 +87,20 @@ public class EnemyAI : MonoBehaviour {
     public float Angle(Vector3 targetDirection)
     {
         return Vector3.Angle(this.transform.forward, targetDirection);
+    }
+
+    public Vector3 Direction(Transform target)
+    {
+        direction = target.position - this.transform.position;
+        direct.position = direction;
+        return direction;
+    }
+
+    private void PassivePathing(Vector3 targetLocation, Transform location1, Transform location2)
+    {
+        if (Vector3.Distance(targetLocation, location1.position) < 0.2f)
+            Direction(location2);
+        else if (Vector3.Distance(targetLocation, location2.position) < 0.2f)
+            Direction(location1);
     }
 }
